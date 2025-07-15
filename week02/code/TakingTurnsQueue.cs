ba5d67c26ef6f3b3895aa43a94687f34689cf293
joshuacirilo <u175692795@byui.edu>
2025-07-14 22:28:48 -0600

    solve part 1

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -45,6 +45,11 @@ public class TakingTurnsQueue
                 person.Turns -= 1;
                 _people.Enqueue(person);
             }
+            else if(person.Turns <= 0)
+            {
+                _people.Enqueue(person); // we are adding again the infinite turns 
+            }
+            
 
             return person;
         }
--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -11,7 +11,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
     // run until the queue is empty
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: the problem is in the Enqueu because we are adding like lifo and not like a fifo if we fix this we wont have problem with this test, because the code is handle the finite repetition 
     public void TestTakingTurnsQueue_FiniteRepetition()
     {
         var bob = new Person("Bob", 2);
@@ -43,7 +43,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3)
     // After running 5 times, add George with 3 turns.  Run until the queue is empty.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
-    // Defect(s) Found: 
+    // Defect(s) Found: if we fix the problem with the Enqueu this test will work 
     public void TestTakingTurnsQueue_AddPlayerMidway()
     {
         var bob = new Person("Bob", 2);
@@ -85,7 +85,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: the program is not working with the foreverzero, so if someone add a zero or less, the code will remove the person and wont add into the queue 
     public void TestTakingTurnsQueue_ForeverZero()
     {
         var timTurns = 0;
@@ -116,7 +116,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: the program is not working with the foreverzero, so if someone add a zero or less, the code will remove the person and wont add into the queue 
     public void TestTakingTurnsQueue_ForeverNegative()
     {
         var timTurns = -3;
@@ -143,7 +143,7 @@ public class TakingTurnsQueueTests
     [TestMethod]
     // Scenario: Try to get the next person from an empty queue
     // Expected Result: Exception should be thrown with appropriate error message.
-    // Defect(s) Found: 
+    // Defect(s) Found: we dont have problem with this because we have a method to know when the queue is empty 
     public void TestTakingTurnsQueue_Empty()
     {
         var players = new TakingTurnsQueue();
